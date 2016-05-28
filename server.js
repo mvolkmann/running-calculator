1fd6ffb73607ac8a72d824d7f82738787f2d0161
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-05-28 12:44:00 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+/*global __dirname, require */
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+//const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json()); // for Content-Type 'application/json'
+app.use(bodyParser.text()); // for Content-Type 'text/plain'
+// for Content-Type 'application/x-www-form-urlencoded'
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname));
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const port = app.get('port');
+app.listen(port, () => console.log('started on port', port));
